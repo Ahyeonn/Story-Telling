2ce6195b036e3139f48019704bdffea11d902c09
Ahyeon Jeon <ahyeon@Ahyeons-MBP.attlocal.net>
2021-10-15 01:17:56 -0700

    update_code

--- python.py
+++ python.py
@@ -13,8 +13,8 @@ def trap_room():
 def home():
     print(display_home)
     print('\n- "I have collected:"', collected_items)
-    print('\n- "All the required ingredients are ready to be turn into a YUMMY strawberry fruit pie." {happy}')
-    print('\n- "Thank you for helping me out to make a most delicious strawberry fruit pie in the world!" 🤩 ') 
+    print(f'\n- "All of the required ingredients are ready to be turned into a YUMMY strawberry fruit pie." {happy}')
+    print('\n- "Thank you for helping me out with making the most delicious strawberry fruit pie in the world!" 🤩 ') 
     print(pie)
     print("\n- You won the game! 👏 👏 👏 \n")
 
@@ -23,13 +23,13 @@ def select_transportation():
     answer = input("    > ").lower()
     if answer == "bicycle":
         print(bicycle)
-        print('\n- "It might take a whole day to get to the grocery store and I hope to get home before it gets dark." 🚲 ')
+        print('\n- "It might take a whole day to get to the place and I hope to get home before it gets dark." 🚲 ')
     elif answer == "bus":
         print(bus)
-        print('\n- "I am glad that I decided to take a bus to get to the grocery store since it is really hot outside." 🚌 ')
+        print('\n- "I am glad that I decided to take the bus to get to the place since it is really hot outside." 🚌 ')
     elif answer == "taxi":
         print(taxi)
-        print('\n- "It might be quiet pricy but I think I can get all the ingredients before the sun goes down." 🚕 ')
+        print('\n- "It might be quite pricy, but I think I can get all of the ingredients before the sun goes down." 🚕 ')
     else:
         select_transportation()
 
@@ -42,13 +42,13 @@ def grocery_room():
         print(f"    - There is a {transportation} to get to the grocery store.")
     select_transportation()
     
-    print('\n- Player arrived to the grocery store 🛒 and went into the enterance.')
+    print('\n- Player arrived at the grocery store 🛒 and went into the entrance.')
     
-    print('\n- "Hi 👋 , Which items are you looking for purchasing today? We just in stock few items but may not have everything that you are looking for." - Grocery Worker')
+    print('\n- "Hi 👋 , Which items are you looking to purchase today? We are in stock for a few items, but may not have everything that you are looking for." - Grocery Worker')
     print('    ----------------------------------------------------------------------------------------------------------')
     print('      |Banana| |Sugar| |Cornstarch| |Baking-Soda| |Whipped-Cream| |Flour| |Chocolate| |Butter| |Milk| |Salt| ')
     print('    ----------------------------------------------------------------------------------------------------------\n')
-    print("- Please type ONE ITEM at the time IDENTICALLY in the list. When you are DONE, type done.")
+    print("- Please type ONE ITEM at a time IDENTICALLY to the list. When you are DONE, type done.")
     while True:
         answer = input("    > ")
         if answer in items_in_store:
@@ -58,31 +58,31 @@ def grocery_room():
             if answer in required_ingredients:
                 required_ingredients.remove(answer)
         elif answer in collected_items:
-            print("\n- You already have this item in the cart.")
+            print("\n- You already have this item in the cart ❗")
         elif "done" in answer:
-            print('\n- "Thank you for using our grocery store. 🛒 See you next time again!" 👋  - Grocery Worker')
+            print('\n- "Thank you for coming to our grocery store. 🛒 See you next time again!" 👋  - Grocery Worker')
             break
         else:
             print('\n- "That item is not available in the store. ❌ "')
     print("\n- These are your remaining required ingredients:", required_ingredients)
     if len(required_ingredients) == 0:
-        print('\n- "Let\'s go back home to make a strawberry fruit pie 🥧 since we collected all the required ingredients"')
+        print('\n- "Let\'s go back home to make a strawberry fruit pie 🥧 since we collected all of the required ingredients."')
         print("\n- How do you want to get to the Mushroom village? 🍄 ")
         select_transportation()
         print(mushroom)
-        print("\n- The player arrived to the Mushroom village. 🍄 ")
-        print("\n- Player arrived home. 🏡 ")
+        print("\n- The player arrived at the Mushroom village. 🍄 ")
+        print("\n- Player has arrived home. 🏡 ")
         home()
     else:
-        print('\n- "We still have to collect few ingredients to make a strawberry fruit pie. Let\'s try to go back to the Mushroom village. 🍄 "')
+        print('\n- "We still have to collect a few ingredients to make a strawberry fruit pie. Let\'s try to go back to the Mushroom village. 🍄 "')
         print("\n- How do you want to get to the Mushroom village? 🍄 ")
         select_transportation()
         print(mushroom)
-        print("\n- The player arrived to the Mushroom village. 🍄 \n")
+        print("\n- The player has arrived at the Mushroom village. 🍄 \n")
         main_room()
 
 def main_room(): 
-    print("- Should I go to the forest 🌲 to get the strawberries? or should I go to the grocery store 🛒 to get other ingredients?")
+    print("- Should I go to the forest 🌲 to get the strawberries or should I go to the grocery store 🛒 to get the other ingredients?")
     answer = input("    > ").lower()
     if answer == "grocery store":
         grocery_room()
@@ -103,19 +103,19 @@ def left_or_right_question(question):
 
 def forest_room():
     print(forest)
-    response = left_or_right_question("\n- There are two ways to get through the forest. Should I go to the left? or should I go to the right?")
+    response = left_or_right_question("\n- There are two ways to get through the forest. Should I go to the left or should I go to the right?")
     if response == "left":
         print('\n- "There isn\'t a way. ⛔  I think we should go back to the forest."')
         forest_room()
     elif response == "right":
-        response = left_or_right_question('\n- There is a footprint 👣 to the left path. Should I go to the left? or should I go to the right?')
+        response = left_or_right_question('\n- There is a footprint 👣 on the left path. Should I go to the left or should I go to the right?')
         if response == "left":
             print('\n- "Oh no! I encounterd a fox! 🦊 I cannot do this anymore. I am going home."')
             trap_room()
         elif response == "right":
-            response = left_or_right_question('\n- I found a small pond 🏞️  and there are two ways to go through the forest. Should I go to the left? or should I go to the right?')
+            response = left_or_right_question('\n- I found a small pond 🏞️  and there are two ways to go through the forest. Should I go to the left or should I go to the right?')
             if response == "left":
-                response = left_or_right_question('\n- I can smell the strawberries 🍓 around here. Which way should I go next? Should I go to the left? or should I go to the right?')
+                response = left_or_right_question('\n- I can smell the strawberries 🍓 around here. Which way should I go next? Should I go to the left or should I go to the right?')
                 if response == "right":
                     if "Strawberries" in required_ingredients:
                         print('\n- "I found the strawberries! 🍓 Now let\'s go back to the mushroom village 🍄 and make sure that we have all the ingredients to bake a strawberry fruit pie."')
@@ -123,20 +123,20 @@ def forest_room():
                         print("\n    - These are your remaining required ingredients:", required_ingredients)
                     else:
                         print("\n- These are your remaining required ingredients:", required_ingredients)
-                        print('\n- "We don\'t need anymore strawberries since we already have collected some❗ "')
+                        print('\n- "We don\'t need anymore strawberries since we have already collected some❗ "')
                     if len(required_ingredients) == 0:
-                        print('\n- "Let\'s go back home 🏠 to bake a strawberry fruit pie since we collected all the required ingredients"')
+                        print('\n- "Since we collected all the required ingredients, let\'s go back home 🏠 to bake a strawberry fruit pie "')
                         home()
                     else:
-                        print('\n- "We still have to collect few required ingredients from the grocery store. 🛒  Let\'s try to go back to the Mushroom village. 🍄 "')
+                        print('\n- "We still have to collect a few required ingredients from the grocery store. 🛒  Let\'s try to go back to the Mushroom village. 🍄 "')
                         print(mushroom)
                         main_room()
 
                 elif response == "left":
-                    print('\n- "Oh no! There are tons of poisonous spiders 🕷️  in here! I need to get out from here before they bite me! 💨 "')
+                    print('\n- "Oh no! There are tons of poisonous spiders 🕷️  in here! I need to get out of here before they bite me! 💨 "')
                     trap_room()
             elif response == "right":
-                print('\n- "Ouchh! I stumbled on the rock! 🪨  I should go back home and disinfect my wound. 🩹 "')
+                print('\n- "Ouchh! I stumbled on a rock! 🪨  I should go back home and disinfect my wound. 🩹 "')
                 trap_room()
 
     else:
@@ -145,7 +145,7 @@ def forest_room():
 
 # Start
 print(hi)
-print("- This is a story-telling game which let users to select few options to choose the path to collect ingredients to make a strawberry fruit pie. 🥧 \n")
+print("- This is a story-telling game which allows users to select from a few options to choose a specific path to collect ingredients in order to make a strawberry fruit pie. 🥧 \n")
 print(f"- These are the ingredients required to make a strawberry fruit pie: {display_required_items}")
-print("    - |Flour 🫓  | & |Butter 🧈 | & |Salt 🧂| are needed to make a crusted pie. \n    - |Strawberries 🍓| & |Sugar 🧂| & |Cornstarch 🌽 | & |Whipped-Cream 🥛| are needed to complete making a Strawberry fruit pie.\n")
+print("    - |Flour 🫓  | & |Butter 🧈 | & |Salt 🧂| are needed to make a pie crust. \n    - |Strawberries 🍓| & |Sugar 🧂| & |Cornstarch 🌽 | & |Whipped-Cream 🥛| are needed to make the topping for the Strawberry fruit pie.\n")
 main_room()
